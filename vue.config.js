182f953573af66bb65fadb2e4da55360a8636225
Alan <alan@salsadigital.com.au>
2020-07-23 13:26:04 +1000

    Fix to image path.

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,6 @@ module.exports = {
   productionSourceMap: false,
   devServer: {
     disableHostCheck: true
-  }
+  },
+  publicPath: process.env.NODE_ENV === 'production' ? 'https://alan-cole.github.io/qld-bsc/': '/'
 }
