fd8575dba76ea1d5a8452d29f945d515b293c730
Alan <alan@salsadigital.com.au>
2022-04-28 15:42:51 +1000

    Update dev environment to PR-53

--- src/libs/settings.js
+++ src/libs/settings.js
@@ -1,7 +1,7 @@
 module.exports = {
   domain: {
     dev: {
-      url: 'https://nginx-php.master.qld-bsc.au2.amazee.io/',
+      url: 'https://nginx-php.pr-53.qld-bsc.au2.amazee.io/',
       auth: { username: 'bsc', password: 'bsc2020' }
     },
     master: {
